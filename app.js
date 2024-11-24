de223e62575e408655dfb74029d2016c1d849db9
Fabrizio Guzmán <fabrizzio86@outlook.es>
2024-11-23 18:47:03 -0600

    feat: Game finished

--- app.js
+++ app.js
@@ -2,27 +2,142 @@ const gameArea = document.getElementById("game-area")
 const player = document.querySelector('#player')
 const beyonce = document.querySelectorAll(`#beyonce`)[0]
 const audio = document.querySelector('audio')
+const playBtn = document.getElementById('play-btn')
+
+// Configuration steps
+let diff = document.getElementById('config-difficulty')
+let theme = document.getElementById('config-theme')
+let backg = document.getElementById('config-backg')
+let char = document.getElementById('config-char')
+
+let title = document.querySelector('h1')
+let config = document.getElementById('config')
+let configTitle = document.getElementById('config-title')
+let game = document.getElementById('game')
 
 const playerSpeed = 35
-const beyonceSpeed = 2
+let beyonceSpeed = 2
 
 let isPlaying = true
 let playerPosition = { x: 0, y: 0 }
 let beyoncePosition = { x: 300, y: 300 }
 
 /**
- * Esta función detecta cuando Beyonce ya te alcanzó
+ * This function shows the initially hidden game
+ */
+function showGame() {
+    game.style.display = 'block'
+
+    // Hiding the configuration section
+    config.style.display = 'none'
+    configTitle.style.display = 'none'
+    playBtn.style.display = 'none'
+    playBtn.disabled = true
+}
+
+/**
+ * 'Difficulty' configuration
+ */
+function goTheme(difficulty) {
+    // Hiding the difficulty configuration window
+    diff.style.display = 'none'
+
+    // Showing the theme configuration
+    theme.style.display = 'flex'
+    theme.style.flexDirection = 'column'
+
+    // Applying the difficulty
+    if (difficulty == 'hard') { beyonceSpeed = 3 }
+}
+
+/**
+ * 'Theme' configuration
+ */
+function goBackground(myTheme) {
+    // Hiding the theme configuration window
+    theme.style.display = 'none'
+
+    // Showing the background configuration
+    backg.style.display = 'flex'
+    backg.style.flexDirection = 'column'
+
+    // Applying the theme
+    if (myTheme == 'dark') {
+        title.style.color = 'white'
+        configTitle.style.color = 'white'
+        config.style.border = '5px solid white'
+        config.style.backgroundColor = '#b5b3b2'
+
+        document.body.style.backgroundColor = '#3d3d3d'
+    }
+}
+
+/**
+ * 'Background' configuration
+ */
+function goCharacter(background) {
+    // Hiding the background configuration window
+    backg.style.display = 'none'
+
+    // Showing the character configuration
+    char.style.display = 'flex'
+    char.style.flexDirection = 'column'
+
+    // Applying the background
+    switch(background) {
+        case 'grass':
+            gameArea.style.backgroundImage = 'url(graphics/grass_back.webp)'
+            break
+        case 'dirt':
+            gameArea.style.backgroundImage = 'url(graphics/dirt_back.webp)'
+            break
+        case 'lava':
+            gameArea.style.backgroundImage = 'url(graphics/lava_back.webp)'
+            break
+        case 'water':
+            gameArea.style.backgroundImage = 'url(graphics/water_back.webp)'
+            break
+    }
+}
+
+/**
+ * 'Character' configuration;
+ * Enabling the play button
+ */
+function enablePlay(character) {
+    // Applying the character
+    switch (character) {
+        case 'steve':
+            player.style.backgroundImage = 'url(graphics/_steve.png)'
+            break
+        case 'alex':
+            player.style.backgroundImage = 'url(graphics/_alex.jpg)'
+            break
+        case 'creeper':
+            player.style.backgroundImage = 'url(graphics/_creeper.png)'
+            break
+        case 'pig':
+            player.style.backgroundImage = 'url(graphics/_pig.png)'
+            break
+    }
+
+    // Enabling the play button
+    playBtn.disabled = false;
+}
+
+/**
+ * This function detects if Beyoncé has caught you
  */
 function detectCollision () {
     const deltaX = Math.abs(playerPosition.x - beyoncePosition.x)
     const deltaY = Math.abs(playerPosition.y - beyoncePosition.y)
 
     if (deltaX <= 50 && deltaY <= 50) {
-        if(confirm('Beyonce te atrapó! Rápido, dale las gracias para salvar tu vida')) {
+        if(confirm('Beyoncé has caught you! Quick, thank her to save your life')) {
             playerPosition.x = Math.floor(Math.random() * (gameArea.clientWidth - 70))
             playerPosition.y = Math.floor(Math.random() * (gameArea.clientHeight - 70))
         } else {
-            alert('Perdiste :(')
+            alert('You lost :(')
             isPlaying = false
             audio.pause()
         }
@@ -79,7 +194,7 @@ function updatePosition () {
 }
 
 window.addEventListener('keydown', movePlayer)
-window.addEventListener('load', () => {
+playBtn.addEventListener('click', () => {
     audio.play()
     gameLoop()
 })
